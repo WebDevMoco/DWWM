42d5ee75ce5c87b347f3caf3ec7fe5872bcce204
Mireille <mireillechaperon@gmail.com>
2020-06-02 18:55:59 +0200

    Ajouté fichiers

---
+++ Formulaires/FormulairesGreta/gretaUsers.php
@@ -0,0 +1,11 @@
+
+<?php
+
+echo "Votre nom : ".$_POST['lastName']."<br>";
+echo "Votre Prénom : ".$_POST['firstName']."<br>";
+echo "Votre adresse mail : ".$_POST['eMail']."<br>";
+echo "Mot de passe : ".$_POST['password']."<br>";
+echo "Controle du Mot de passe : ".$_POST['controlPassword']."<br>";
+
+
+?>
